2584d9cd04f5608ae33e07734ab128cc7360cde5
vdtch <51941092+vdtch@users.noreply.github.com>
2019-09-02 00:13:00 +0200

    calcEngine with accessors and mutators

--- src/com/vdtch/calcengine/Main.java
+++ src/com/vdtch/calcengine/Main.java
@@ -4,11 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        //double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        //char[] opCodes = {'d', 'a', 's', 'm'};
-        //double[] results = new double[opCodes.length];
-
         MathEquation[] equations = new MathEquation[4];
         equations[0] = create(100.d, 50.0d, 'd');
         equations[1] = create(25.d, 92.0d, 'a');
@@ -18,15 +13,15 @@ public class Main {
         for (MathEquation equation : equations) {
             equation.execute();
             System.out.println("result =");
-            System.out.println(equation.result);
+            System.out.println(equation.getResult());
         }
     }
 
     public static MathEquation create(double leftVal, double rightVal, char opCode) {
         MathEquation equation = new MathEquation();
-        equation.leftVal = leftVal;
-        equation.rightVal = rightVal;
-        equation.opCode = opCode;
+        equation.setLeftVal(leftVal);
+        equation.setRightVal(rightVal);
+        equation.setOpCode(opCode);
 
         return equation;
     }
--- src/com/vdtch/calcengine/MathEquation.java
+++ src/com/vdtch/calcengine/MathEquation.java
@@ -1,10 +1,20 @@
 package com.vdtch.calcengine;
 
 public class MathEquation {
-    public double leftVal;
-    public double rightVal;
-    public char opCode;
-    public double result;
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
+
+    public double getLeftVal(){return leftVal;}
+    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+    public double getRightVal(){return rightVal;}
+    public void setRightVal (double rightVal) {this.rightVal = rightVal;}
+    public char getOpcode(){return opCode;}
+    public void setOpCode (char opCode) {this.opCode = opCode;}
+
+    public double getResult(){return result;}
+
 
     public void execute () {
         switch (opCode) {
