6a9e7b7c7ae28899b6209e12219616e47cc02c8b
vdtch <51941092+vdtch@users.noreply.github.com>
2019-09-10 06:27:58 +0200

    Update

--- src/com/vdtch/calcengine/Main.java
+++ src/com/vdtch/calcengine/Main.java
@@ -5,24 +5,40 @@ public class Main {
     public static void main(String[] args) {
 
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.d, 50.0d, 'd');
-        equations[1] = create(25.d, 92.0d, 'a');
-        equations[2] = create(225.d, 17.0d, 's');
-        equations[3] = create(11.d, 3.0d, 'm');
+        equations[0] = new MathEquation('d', 100.d, 50.0d);
+        equations[1] = new MathEquation('a',25.d, 92.0d);
+        equations[2] = new MathEquation('s', 225.d, 17.0d);
+        equations[3] = new MathEquation('m',11.d, 3.0d);
 
         for (MathEquation equation : equations) {
             equation.execute();
             System.out.println("result =");
             System.out.println(equation.getResult());
         }
+
+        System.out.println();
+        System.out.println("Using Overload");
+        System.out.println();
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        int leftInt= 9;
+        int rightInt = 4;
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.println("Results =");
+        System.out.println(equationOverload.getResult());
+
+        equationOverload.execute(leftInt, rightInt);
+        System.out.println("Results =");
+        System.out.println(equationOverload.getResult());
+
+        equationOverload.execute((double)leftInt, rightInt);
+        System.out.println("Results =");
+        System.out.println(equationOverload.getResult());
     }
 
-    public static MathEquation create(double leftVal, double rightVal, char opCode) {
-        MathEquation equation = new MathEquation();
-        equation.setLeftVal(leftVal);
-        equation.setRightVal(rightVal);
-        equation.setOpCode(opCode);
 
-        return equation;
-    }
 }
--- src/com/vdtch/calcengine/MathEquation.java
+++ src/com/vdtch/calcengine/MathEquation.java
@@ -15,6 +15,33 @@ public class MathEquation {
 
     public double getResult(){return result;}
 
+    public MathEquation(){}
+    
+    public MathEquation(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal){
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
+
+    public void execute(double leftVal, double rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+    }
+
+    public void execute(int leftVal, int rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+        result = (int)result;
+    }
+
 
     public void execute () {
         switch (opCode) {
